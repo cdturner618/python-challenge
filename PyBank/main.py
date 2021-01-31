0f29c1143fb2e126d7a10c21c3158e4e251c9e78
cdturner618 <cdturner618@gmail.com>
2021-01-30 20:53:29 -0600

    To print each row

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,5 @@
 # The total number of months included in the dataset
+import csv
 total_months = 0
 
 # The net total amount of "Profit/Losses" over the entire period
@@ -13,12 +14,22 @@ greates_increase = {}
 # The greatest decrease in losses(date and amount) over the entire period
 greates_decrease = {}
 
+file_path = "./Resoruces/Budget_data.csv"
+with open(file_path) as csvfile:
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile)
+    # Read the header row first(skip this step if there is no header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
 
-# Results shoud look like this
-# Financial Analysis
-# ----------------------------
-# Total Months: 86
-# Total: $38382578
-# Average  Change: $-2315.12
-# Greatest Increase in Profits: Feb-2012 ($1926159)
-# Greatest Decrease in Profits: Sep-2013 ($-2196167)
+        # Results shoud look like this
+        # Financial Analysis
+        # ----------------------------
+        # Total Months: 86
+        # Total: $38382578
+        # Average  Change: $-2315.12
+        # Greatest Increase in Profits: Feb-2012 ($1926159)
+        # Greatest Decrease in Profits: Sep-2013 ($-2196167)
