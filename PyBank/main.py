8a4135dbccd7f56575917b1c7490f8c2e0eddb9f
cdturner618 <cdturner618@gmail.com>
2021-02-06 08:21:37 -0600

    should print total

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,6 +6,7 @@ average_profit_loss = 0.00
 greatest_increase = {"date": "", "amount": 0}
 greatest_decrease = {"date": "", "amount": 0}
 
+
 file_path = "./Resources/Budget_data.csv"
 
 with open(file_path) as csvfile:
@@ -20,12 +21,12 @@ with open(file_path) as csvfile:
         total_months = total_months + 1
         date = row[0]
         profit = float(row[1])
-        # The net total amount of "Profit/Losses" over the entire period
-        total_profit_loss = profit
-        print(math.fsum(profit))
 
-        # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-        # average_profit_loss =
+        # The net total amount of "Profit/Losses" over the entire period
+        total_profit_loss = total_profit_loss + profit
+
+        # calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+
         # The greatest increase in profits(date and amount) over the entire period
         if (profit > greatest_increase["amount"]):
             greatest_increase["date"] = date
@@ -39,17 +40,17 @@ with open(file_path) as csvfile:
 print("Finacial Analysis")
 print("----------------------------------")
 print(f"Total Months: {total_months}")
-print(f"Total:{total_profit_loss}")
+print(f"Total: {total_profit_loss}")
 print(
     f"Greatest Increase in Profits:{greatest_increase['date']} (${greatest_increase['amount']})")
 print(
     f"Greatest Decrease in Profits:{greatest_decrease['date']} (${greatest_decrease['amount']})")
 
-# Results shoud look like this
-# Financial Analysis
-# ----------------------------
-# Total Months: 86
-# Total: $38382578
-# Average  Change: $-2315.12
-# Greatest Increase in Profits: Feb-2012 ($1926159)
-# Greatest Decrease in Profits: Sep-2013 ($-2196167)
+# # Results shoud look like this
+# # Financial Analysis
+# # ----------------------------
+# # Total Months: 86
+# # Total: $38382578
+# # Average  Change: $-2315.12
+# # Greatest Increase in Profits: Feb-2012 ($1926159)
+# # Greatest Decrease in Profits: Sep-2013 ($-2196167)
