ad382f487dc40b8368db31a09d2bd8342edd84d2
cdturner618 <cdturner618@gmail.com>
2021-02-09 10:53:29 -0600

    write to output file complete

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,12 +3,8 @@ total_votes = 0
 candidates_dictionary = {}
 Name = ""
 
-# voter_id = float(row[0])
-# country = row[1]
-# candidate_row = {}
-# Winner = {"Voter ID": "", "Candidate": 0}
-
 file_path = "./Resources/election_data.csv"
+output_file = "./Analysis/output.txt"
 
 with open(file_path) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
@@ -34,7 +30,6 @@ print("----------------------------------")
 print(f"Total Votes: {total_votes}")
 print("----------------------------------")
 # The percentage of votes each candidate won
-
 # The total number of votes each candidate won
 for key, value in candidates_dictionary.items():
     print(f"{key}: {round(value / total_votes*100, 2)}% ({value})")
@@ -43,7 +38,21 @@ for key, value in candidates_dictionary.items():
 print("----------------------------------")
 print(f"Winner: {winner[0]}")
 print("----------------------------------")
-# print(f"List of Candidates: {candidate}")
+
+# Write to a file
+with open(output_file, 'w') as outputFile:
+    outputFile.write("Election Results")
+    outputFile.write("----------------------------------\n")
+    outputFile.write(f"Total Votes: {total_votes}\n")
+    outputFile.write("----------------------------------\n")
+    for key, value in candidates_dictionary.items():
+        outputFile.write(
+            f"{key}: {round(value / total_votes*100, 2)}% ({value})\n")
+    outputFile.write("----------------------------------\n")
+    outputFile.write(f"Winner: {winner[0]}\n")
+    outputFile.write("----------------------------------\n")
+
+
 # Results shoud look like this
 # Election Results
 # -------------------------
