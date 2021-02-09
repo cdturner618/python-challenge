75ca39fb2eee4f4ef72bc1c824be93e9c26656af
cdturner618 <cdturner618@gmail.com>
2021-02-09 09:42:59 -0600

    Write to file

--- PyBank/main.py
+++ PyBank/main.py
@@ -57,7 +57,15 @@ print(
 # Write to a file
 with open(output_file, 'w') as outputFile:
     outputFile.write("Finacial Analysis")
-
+    outputFile.write("----------------------------------")
+    outputFile.write(f"Total Months: {total_months}")
+    outputFile.write(f"Total: {total_profit_loss}")
+    outputFile.write(
+        f"Average:{round(sum(change_profit_list) / (total_months - 1), 2)}")
+    outputFile.write(
+        f"Greatest Increase in Profits:{greatest_increase['date']} (${greatest_increase['amount']})")
+    outputFile.write(
+        f"Greatest Decrease in Profits:{greatest_decrease['date']} (${greatest_decrease['amount']})")
 
 # # Results shoud look like this
 # # Financial Analysis
